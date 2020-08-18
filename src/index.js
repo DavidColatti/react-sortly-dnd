ac4623e50957c81adb0c84bcc83868cd5fe04bf2
David Colatti <davidcolatti@gmail.com>
2020-08-18 11:44:58 -0400

    feat: testing sortly

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import Sortly, { ContextProvider, useDrag, useDrop } from "react-sortly";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const ItemRenderer = (props) => {
+  const {
+    data: { name, depth },
+  } = props;
+
+  const [, drag] = useDrag();
+  const [, drop] = useDrop();
+
+  return (
+    <div ref={drop}>
+      <div ref={drag} style={{ marginLeft: depth * 20 }}>
+        {name}
+      </div>
+    </div>
+  );
+};
+
+const MySortableTree = () => {
+  const [items, setItems] = useState([
+    { id: 1, name: "Priscilla Cormier", depth: 0 },
+    { id: 2, name: "Miss Erich Bartoletti", depth: 0 },
+    { id: 3, name: "Alison Friesen", depth: 1 },
+    { id: 4, name: "Bernita Mayert", depth: 2 },
+    { id: 5, name: "Garfield Berge", depth: 0 },
+  ]);
+  const handleChange = (newItems) => {
+    setItems(newItems);
+  };
+
+  return (
+    <Sortly items={items} onChange={handleChange}>
+      {(props) => <ItemRenderer {...props} />}
+    </Sortly>
+  );
+};
+
+const App = () => (
+  <DndProvider backend={HTML5Backend}>
+    <ContextProvider>
+      <MySortableTree />
+    </ContextProvider>
+  </DndProvider>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
